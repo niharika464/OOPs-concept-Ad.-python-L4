7bc0499213c4d67ebdba2671367c68784beb65e2
niharika464 <niharikasinghgaur11@gmail.com>
2025-03-06 23:22:26 +0530

    constructor and destructor

---
+++ condes.py
@@ -0,0 +1,11 @@
+class student:
+      def __init__(self):#constructor
+            print("students details are created")
+
+
+      def __del__(self):#destructor 
+            print("students details are created")
+
+
+s=student()
+del s                  
